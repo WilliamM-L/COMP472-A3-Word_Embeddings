06e091c625a72d57ec1f13680a71f4d7b6453adf
WilliamM-L <w.morin.laberge@gmail.com>
2021-11-21 01:19:41 -0500

    Task 1 complete

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import gensim.downloader
 
 
 def test_single_model(model_name='word2vec-google-news-300'):
+    correct_counter = 0
+    non_guessing_counter = 0
     with open(f'logs\\{model_name}-details.csv', 'w') as logger:
-        guess_count = 0
-        correct_counter = 0
         gensim.downloader.BASE_DIR = gensim_data_path
         model = gensim.downloader.load(model_name)
         with open('datasets\\synonyms.csv') as dataset_file:
@@ -34,9 +34,20 @@ def test_single_model(model_name='word2vec-google-news-300'):
                 try:
                     similarities = model.distances(question, words_to_compare)
                     print(similarities)
+                    non_guessing_counter += 1
                     lowest_score = 2
-                    for score in similarities:
-                        print(score)
+                    model_guess_index = 8
+                    for index, score in enumerate(similarities):
+                        if score < lowest_score:
+                            lowest_score = score
+                            model_guess_index = index
+
+                    if model_guess_index == answer_index:
+                        correct_counter += 1
+                        label = Label.Correct
+                    else:
+                        label = Label.Wrong
+
                 except KeyError:
                     # will get here if any words in the list are not part of the model, must check each one manually :(
                     print('Could not find word!')
@@ -50,8 +61,6 @@ def test_single_model(model_name='word2vec-google-news-300'):
                         if score < lowest_score:
                             lowest_score = score
                             model_guess_index = index
-                    # no matter if we get it right or not, we've been guessing since we don't know all words
-                    guess_count += 1
 
                     if lowest_score == 2:
                         # got a KeyError for every invocation -> answer is unknown or all 4 guess words are unknown
@@ -62,8 +71,10 @@ def test_single_model(model_name='word2vec-google-news-300'):
                         if model_guess_index == answer_index:
                             label = Label.Correct
                             correct_counter += 1
-            logger.write(f'{question},{answer},{words_to_compare[model_guess_index]},{str(label)}')
+                logger.write(f'{question},{answer},{words_to_compare[model_guess_index]},{label}\n')
 
+    with open('logs\\analysis.csv', 'a') as analysis_logger:
+        analysis_logger.write(f'{model_name},{len(model)},{correct_counter},{non_guessing_counter},{correct_counter/non_guessing_counter}\n')
 
 
 def main():
